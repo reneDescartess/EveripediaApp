31e8ede53490bf4352aa3181d82266bbb5a1c570
Christian Deciga <everipedia_iMac@everipedia-imacs-imac.local>
2016-05-06 01:22:08 -0700

    Initial Commit

---
+++ Everipedia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Everipedia
+//
+//  Created by everipedia_iMac on 5/6/16.
+//  Copyright © 2016 Christian Deciga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
